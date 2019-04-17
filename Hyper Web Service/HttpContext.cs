0f98eb6a97acc79398eb0125ff2e1fcdd8a5fd9b
Narrakas <zixing.narrakas1996@gmail.com>
2019-04-17 18:04:47 -0400

    Added missing docs in \Hyper Web Service\HttpContext.c

--- Hyper Web Service/HttpContext.cs
+++ Hyper Web Service/HttpContext.cs
@@ -1,8 +1,15 @@
 ﻿namespace Intellivoid.HyperWS
 {
     
+    /// <summary>
+    /// HttpContext Class
+    /// </summary>
     public class HttpContext
     {
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        /// <param name="client"></param>
         internal HttpContext(HttpClient client)
         {
             Server = client.Server.ServerUtility;
@@ -10,10 +17,19 @@
             Response = new HttpResponse(this);
         }
 
+        /// <summary>
+        /// HTTP Server Utility
+        /// </summary>
         public HttpServerUtility Server { get; private set; }
 
+        /// <summary>
+        /// HTTP Request
+        /// </summary>
         public HttpRequest Request { get; private set; }
 
+        /// <summary>
+        /// HTTP Response
+        /// </summary>
         public HttpResponse Response { get; private set; }
     }
 }
